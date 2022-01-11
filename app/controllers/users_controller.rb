e9298bf2465f6c4a5c086fbc09f897bcb810f472
yanagiya-umi(imac) <yana3umi@gmail.com>
2022-01-11 23:58:23 +0900

    edit[ajax]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,7 @@ class UsersController < ApplicationController
   private
    def user_params
     params.require(:user).permit(:name,:introduction,:profile_image)
-   end
+   printbinary 
+  end
      
 end
